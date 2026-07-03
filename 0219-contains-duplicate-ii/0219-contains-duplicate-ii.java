90f7d3f2e836e585bc23a11cd4c59c834b9e7307
Vishnupreethi B <vishnupreethi084@gmail.com>
2026-07-03 22:29:42 +0530

    Time: 1164 ms (6.93%), Space: 70.2 MB (95.36%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,8 +1,11 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        for(int i=0;i<nums.length;i++){
-            for(int j = i+1; j <= i + k &&j<nums.length;j++){
-                if(nums[i]==nums[j] && Math.abs(i - j)<=k){
+        for (int i = 0; i < nums.length - 1; i++){
+            for (int j = i+1; j < nums.length; j++){
+                if (j - i > k){
+                    break;
+                }
+                if (nums[j] == nums[i]){
                     return true;
                 }
             }
