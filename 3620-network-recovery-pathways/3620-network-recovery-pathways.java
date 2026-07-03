2ea8d196848a16bdc63ef1e865ca42f37c9c26ac
Vishnupreethi B <vishnupreethi084@gmail.com>
2026-07-03 21:39:20 +0530

    Time: 14 ms (98.02%), Space: 160.3 MB (83.17%) - LeetHub

--- 3620-network-recovery-pathways/3620-network-recovery-pathways.java
+++ 3620-network-recovery-pathways/3620-network-recovery-pathways.java
@@ -1,77 +1,73 @@
-import java.util.*;
-
 class Solution {
-    List<int[]>[] g;
-    int[] topo;
-    boolean[] on;
-    long k;
-
+    private int[] head, next, to, weight;
+    private long k;
+    private int n;
     public int findMaxPathScore(int[][] edges, boolean[] online, long k) {
-        int n = online.length;
+        this.n = online.length;
         this.k = k;
-        this.on = online;
 
-        g = new ArrayList[n];
-        int[] indegree = new int[n];
-        for (int i = 0; i < n; i++) g[i] = new ArrayList<>();
+        int m = edges.length;
+        this.head = new int[n];
+        this.next = new int[m];
+        this.to = new int[m];
+        this.weight = new int[m];
+        Arrays.fill(head, -1);
+        int left = Integer.MAX_VALUE, right = 0;
+        for(int i = 0; i < m; i++) {
+            int a = edges[i][0], b = edges[i][1], c = edges[i][2];
+            if(online[a] && online[b]) {
+                to[i] = b;
+                next[i] = head[a];
+                weight[i] = c;
+                head[a] = i;
 
-        int hi = 0;
-        for (int[] e : edges) {
-            g[e[0]].add(new int[]{e[1], e[2]});
-            indegree[e[1]]++;
-            hi = Math.max(hi, e[2]);
-        }
-
-        topo = new int[n];
-        Queue<Integer> q = new ArrayDeque<>();
-        for (int i = 0; i < n; i++)
-            if (indegree[i] == 0) q.offer(i);
-
-        int idx = 0;
-        while (!q.isEmpty()) {
-            int u = q.poll();
-            topo[idx++] = u;
-            for (int[] e : g[u]) {
-                if (--indegree[e[0]] == 0)
-                    q.offer(e[0]);
+                if(c > right) right = c;
+                if(c < left) left = c;
             }
         }
 
-        int l = 0, r = hi, ans = -1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (check(mid, n)) {
-                ans = mid;
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
-        }
+        if(!check(0)) return -1;
 
-        return ans;
+        while(left < right) {
+            int mid = left + right + 1 >>> 1;
+            if(check(mid)) left = mid;
+            else right = mid - 1;
+        }
+        return left;
     }
 
-    private boolean check(int x, int n) {
-        long INF = Long.MAX_VALUE / 2;
+    private static final int[] queue = new int[50001];
+    private static final long[] sum = new long[50001];
+    private boolean check(int threshold) {
         long[] dist = new long[n];
-        Arrays.fill(dist, INF);
+        Arrays.fill(dist, k + 1);
         dist[0] = 0;
 
-        for (int u : topo) {
-            if (dist[u] == INF) continue;
-            if (u != 0 && u != n - 1 && !on[u]) continue;
-
-            for (int[] e : g[u]) {
-                int v = e[0], w = e[1];
-
-                if (w < x) continue;
-                if (v != n - 1 && !on[v]) continue;
-
-                if (dist[v] > dist[u] + w)
-                    dist[v] = dist[u] + w;
+        int read = 0, write = 1;
+        while(read < write) {
+            int current = queue[read];
+            long val = sum[read++];
+            if(val > dist[current]) continue;
+            for(int i = head[current]; i != -1; i = next[i]) {
+                if(weight[i] < threshold) continue;
+                int nextIndex = to[i];
+                long d = val + weight[i];
+                if(d < dist[nextIndex]) {
+                    if(nextIndex == n - 1) return true;
+                    dist[nextIndex] = d;
+                    sum[write] = d;
+                    queue[write++] = nextIndex;
+                }
             }
         }
-
-        return dist[n - 1] <= k;
+        return false;
+    }
+    private static final class Node {
+        private final int index;
+        private final long dist;
+        private Node(int index, long dist) {
+            this.index = index;
+            this.dist = dist;
+        }
     }
 }
