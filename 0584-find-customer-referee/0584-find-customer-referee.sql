4d5e0dfc5b3f24bac594bc8249fbabff001b6002
Vishnupreethi B <vishnupreethi084@gmail.com>
2026-07-07 20:37:33 +0530

    Time: 471 ms (87.34%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,2 +1,2 @@
-select name from Customer
- where referee_id!=2 or referee_id is null ;
+# Write your MySQL query statement below
+select name from Customer where referee_id!=2 or referee_id is null;
