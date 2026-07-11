9f4b551461a51cd2a00a348b5c60630efb79e9a9
Vishnupreethi B <vishnupreethi084@gmail.com>
2026-07-11 16:31:49 +0530

    Time: 4 ms (99.67%), Space: 47.5 MB (88.79%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
@@ -1,44 +1,58 @@
 class Solution {
-    List<Integer>[] graph;
-    boolean[] vis;
-    int nodes, degreeSum;
-
     public int countCompleteComponents(int n, int[][] edges) {
-        graph = new ArrayList[n];
-        vis = new boolean[n];
-
-        for (int i = 0; i < n; i++)
-            graph[i] = new ArrayList<>();
-
-        for (int[] e : edges) {
-            graph[e[0]].add(e[1]);
-            graph[e[1]].add(e[0]);
-        }
-
-        int ans = 0;
-
+        int[] parent = new int[n];
+        int[] nodeCount = new int[n];
+        int[] edgeCount = new int[n];
+        
+        // Initialize each node to be its own parent, with 1 node and 0 edges
         for (int i = 0; i < n; i++) {
-            if (!vis[i]) {
-                nodes = 0;
-                degreeSum = 0;
-                dfs(i);
-
-                if (degreeSum / 2 == nodes * (nodes - 1) / 2)
-                    ans++;
+            parent[i] = i;
+            nodeCount[i] = 1;
+        }
+        
+        // Union find tracking
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            
+            int rootU = find(u, parent);
+            int rootV = find(v, parent);
+            
+            if (rootU != rootV) {
+                // Merge group V into group U
+                parent[rootV] = rootU;
+                nodeCount[rootU] += nodeCount[rootV];
+                edgeCount[rootU] += edgeCount[rootV] + 1; // Add this new connecting edge
+            } else {
+                // u and v are already in the same group; this is a redundant edge within it
+                edgeCount[rootU]++;
             }
         }
-
-        return ans;
-    }
-
-    private void dfs(int u) {
-        vis[u] = true;
-        nodes++;
-        degreeSum += graph[u].size();
-
-        for (int v : graph[u]) {
-            if (!vis[v])
-                dfs(v);
+        
+        int completeComponents = 0;
+        
+        // Check the roots of all components
+        for (int i = 0; i < n; i++) {
+            if (parent[i] == i) { // This node is the leader of its component
+                int nodes = nodeCount[i];
+                int totalEdges = edgeCount[i];
+                
+                // Since DSU processes edges once, no double counting occurs.
+                // Formula: E == V * (V - 1) / 2
+                if (totalEdges == (nodes * (nodes - 1)) / 2) {
+                    completeComponents++;
+                }
+            }
         }
+        
+        return completeComponents;
+    }
+    
+    // Find with path compression
+    private int find(int i, int[] parent) {
+        if (parent[i] == i) {
+            return i;
+        }
+        return parent[i] = find(parent[i], parent);
     }
 }
