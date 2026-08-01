ecf63a2705cd273f5957e8174a5c7dcdb3ebcd83
Vishnupreethi B <vishnupreethi084@gmail.com>
2026-08-01 22:22:44 +0530

    Time: 0 ms (100%), Space: 42.6 MB (74.96%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        int dp[] = new int[n];
+
+        for(int i = 0;i<n;i++){
+            dp[i] = nums[i];
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j = i+1;j<n;j++){
+                dp[j] = Math.max(nums[i]-dp[j],nums[j]-dp[j-1]);
+            }
+        }
+        return dp[n-1]>=0;
+    }
+}
