396ef50f49e7125af729eda44ddab0774787bfad
Vishnupreethi B <vishnupreethi084@gmail.com>
2026-08-13 18:13:55 +0530

    Time: 1 ms (99.95%), Space: 45.9 MB (97.84%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -2,18 +2,12 @@ class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         int[] count = new int[26];
 
-        // Count characters in magazine
-        for (char c : magazine.toCharArray()) {
+        for (char c : magazine.toCharArray())
             count[c - 'a']++;
-        }
 
-        // Use characters for ransomNote
         for (char c : ransomNote.toCharArray()) {
-            if (count[c - 'a'] == 0) {
+            if (--count[c - 'a'] < 0)
                 return false;
-            } else {
-                count[c - 'a']--;
-            }
         }
 
         return true;
