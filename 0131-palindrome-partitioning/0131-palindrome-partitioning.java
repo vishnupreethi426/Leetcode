762275d872d22547d8605911f7537c4ccf5aec04
Vishnupreethi B <vishnupreethi084@gmail.com>
2026-09-06 20:41:40 +0530

    Time: 6 ms (98.63%), Space: 65.1 MB (75.49%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,60 +1,45 @@
-import java.util.*;
-
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> result = new ArrayList<>();
-        generate(s, 0, new ArrayList<>(), result);
+        backtrack(s, 0, new ArrayList<>(), result);
         return result;
     }
 
-    private void generate(String s, int start,
-                          List<String> current,
-                          List<List<String>> result) {
+    private void backtrack(String s, int start,
+                            List<String> current,
+                            List<List<String>> result) {
 
-        // If all characters are used
+        // All characters are used
         if (start == s.length()) {
-
-            // Check whether the complete partition is valid
-            boolean valid = true;
-
-            for (String str : current) {
-                if (!isPalindrome(str)) {
-                    valid = false;
-                    break;
-                }
-            }
-
-            if (valid) {
-                result.add(new ArrayList<>(current));
-            }
-
+            result.add(new ArrayList<>(current));
             return;
         }
 
-        // Try every possible substring
+        // Try every possible substring starting from start
         for (int end = start; end < s.length(); end++) {
 
-            current.add(s.substring(start, end + 1));
+            if (isPalindrome(s, start, end)) {
 
-            generate(s, end + 1, current, result);
+                // Choose
+                current.add(s.substring(start, end + 1));
 
-            current.remove(current.size() - 1);
+                // Explore
+                backtrack(s, end + 1, current, result);
+
+                // Undo choice
+                current.remove(current.size() - 1);
+            }
         }
     }
 
-    private boolean isPalindrome(String str) {
-        int left = 0;
-        int right = str.length() - 1;
-
+    private boolean isPalindrome(String s, int left, int right) {
         while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
+            if (s.charAt(left) != s.charAt(right)) {
                 return false;
             }
-
             left++;
             right--;
         }
-
         return true;
     }
 }
