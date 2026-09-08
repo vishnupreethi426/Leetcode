9843b5882dc1eae908f7298f9980da0ea163d3ba
Vishnupreethi B <vishnupreethi084@gmail.com>
2026-09-08 21:12:01 +0530

    Time: 0 ms (100%), Space: 42.7 MB (39.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,11 +1,9 @@
 class Solution {
     public int countCommas(int n) {
-        int count = 0;
-        for(int i=1;i<=n;i++){
-            if(i>=1000 && i<=999999){
-                count++;
-            }
+        if (n < 1000) {
+            return 0;
         }
-        return count;
+
+        return n - 999;
     }
 }
