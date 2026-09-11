46b0091c56f01b0772c6e7f0a2650a5ea5e56097
Vishnupreethi B <vishnupreethi084@gmail.com>
2026-09-11 21:18:36 +0530

    Time: 10 ms (24%), Space: 46.9 MB (11.87%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -2,31 +2,25 @@ class Solution {
     public int totalNumbers(int[] digits) {
         int count = 0;
 
-        for (int num = 100; num <= 999; num++) {
+        for (int i = 100; i <= 999; i++) {
+            if (i % 2 != 0) continue;
 
-            // Number must be even
-            if (num % 2 != 0) {
-                continue;
-            }
+            int a = i / 100;
+            int b = (i / 10) % 10;
+            int c = i % 10;
 
-            int a = num / 100;
-            int b = (num / 10) % 10;
-            int c = num % 10;
+            int[] f = new int[10];
 
-            int[] temp = new int[10];
+            for (int d : digits)
+                f[d]++;
 
-            // Count digits available in input
-            for (int digit : digits) {
-                temp[digit]++;
-            }
-
-            // Use one copy of each digit
-            temp[a]--;
-            temp[b]--;
-            temp[c]--;
-
-            if (temp[a] >= 0 && temp[b] >= 0 && temp[c] >= 0) {
-                count++;
+            if (f[a] > 0) {
+                f[a]--;
+                if (f[b] > 0) {
+                    f[b]--;
+                    if (f[c] > 0)
+                        count++;
+                }
             }
         }
 
