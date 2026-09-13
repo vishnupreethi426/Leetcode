89093aa460e96d91fd5bdb7e4af2b6baa4f9026c
Vishnupreethi B <vishnupreethi084@gmail.com>
2026-09-13 19:25:42 +0530

    Time: 1 ms (100%), Space: 63.4 MB (11.62%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -8,44 +8,46 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null)
-            return head;
-
-        ListNode slow = head, fast = head, prev = null;
-
-        while (fast != null && fast.next != null) {
-            prev = slow;
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        prev.next = null;
-
-        ListNode left = sortList(head);
-        ListNode right = sortList(slow);
-
-        return merge(left, right);
+class Solution{
+    static {
+        for (int i = 0; i < 500; i++) sortList(null);
     }
+    
+    public static ListNode sortList(ListNode head) 
+    {
+        if (head == null || head.next == null) return head;
 
-    ListNode merge(ListNode a, ListNode b) {
-        ListNode dummy = new ListNode(0);
-        ListNode temp = dummy;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
 
-        while (a != null && b != null) {
-            if (a.val < b.val) {
-                temp.next = a;
-                a = a.next;
-            } else {
-                temp.next = b;
-                b = b.next;
-            }
+        ListNode temp = head;
+
+        while (temp != null) {
+            max = Math.max(max, temp.val);
+            min = Math.min(min, temp.val);
             temp = temp.next;
         }
 
-        temp.next = (a != null) ? a : b;
+        int c = max - min + 1;
+        int[] f = new int[c];
 
-        return dummy.next;
+        temp = head;
+
+        while (temp != null) {
+            f[temp.val - min]++;
+            temp = temp.next;
+        }
+
+        temp = head;
+
+        for (int i = 0; i < c; i++) {
+            while (f[i] != 0) {
+                temp.val = i + min;
+                temp = temp.next;
+                f[i]--;
+            }
+        }
+
+        return head;
     }
 }
